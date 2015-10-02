89681fcfbfdcdd65b95d14df839c40776e7930f6
Conner D <connerdunn7399@gmail.com>
2015-10-01 23:00:04 -0600

    progress on start method

--- app/src/main/java/com/nettapper/comp301assn1/Game.java
+++ app/src/main/java/com/nettapper/comp301assn1/Game.java
@@ -1,5 +1,7 @@
 package com.nettapper.comp301assn1;
 
+import java.util.Random;
+
 /**
  * Created by Conner on 15-10-01.
  */
@@ -8,7 +10,16 @@ public class Game {
 
     public void start(){
         // wait rand time between 10 ms and 2000 ms
+        Random r = new Random();
+        int sleepTime = (int)(1990 * r.nextDouble() + 10);
+        // Oct 1 2015, Konrad Garus, http://stackoverflow.com/questions/3342651/how-can-i-delay-a-java-program-for-a-few-seconds
+        try {
+            Thread.sleep(sleepTime);  // time is in ms
+        } catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
         // set the initial time var
+        // Oct 1 2015, Kevin Bourrillion, http://stackoverflow.com/questions/1770010/how-do-i-measure-time-elapsed-in-java
         startTime = System.nanoTime();
 
     }
