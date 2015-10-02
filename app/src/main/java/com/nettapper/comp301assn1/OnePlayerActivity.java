c012c91c3a60e5306dcba44d098e5ccf17c4b43a
Conner D <connerdunn7399@gmail.com>
2015-10-01 23:12:21 -0600

    Added a dialog to welcome a user

--- app/src/main/java/com/nettapper/comp301assn1/OnePlayerActivity.java
+++ app/src/main/java/com/nettapper/comp301assn1/OnePlayerActivity.java
@@ -1,9 +1,12 @@
 package com.nettapper.comp301assn1;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class OnePlayerActivity extends AppCompatActivity {
 
@@ -11,5 +14,25 @@ public class OnePlayerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one_player);
+        runTutorial();
+    }
+
+    private void runTutorial(){
+        // Oct 1 2015, http://www.tutorialspoint.com/android/android_alert_dialoges.htm
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("After clicking OK the game will start. Try to click the button on screen as quickly as possible when it changes color!");
+
+        alertDialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface arg0, int arg1) {
+                Toast.makeText(OnePlayerActivity.this, "Starting the game!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+        // End of Tutorialspoint
     }
 }
+
+
