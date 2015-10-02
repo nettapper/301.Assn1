5afcbe22e00ceb904531fe573804b240c0c433bf
Conner D <connerdunn7399@gmail.com>
2015-10-01 23:30:29 -0600

    the game has started before the dialog is closed

--- app/src/main/java/com/nettapper/comp301assn1/OnePlayerActivity.java
+++ app/src/main/java/com/nettapper/comp301assn1/OnePlayerActivity.java
@@ -2,10 +2,14 @@ package com.nettapper.comp301assn1;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class OnePlayerActivity extends AppCompatActivity {
@@ -15,6 +19,7 @@ public class OnePlayerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one_player);
         runTutorial();
+        playGame();
     }
 
     private void runTutorial(){
@@ -32,6 +37,26 @@ public class OnePlayerActivity extends AppCompatActivity {
         AlertDialog alertDialog = alertDialogBuilder.create();
         alertDialog.show();
         // End of Tutorialspoint
+    }
+
+    private void playGame(){
+        Button onePlayerButton = (Button) findViewById(R.id.onePlayer_clickMe);
+        // Oct 1 2015, Hamzeh Soboh, http://stackoverflow.com/questions/12615720/setbackgroundcolor-in-android
+        onePlayerButton.setBackgroundColor(Color.BLUE);
+        final Game game = new Game();
+        game.start();
+
+        // change button color
+        // Oct 1 2015, Hamzeh Soboh, http://stackoverflow.com/questions/12615720/setbackgroundcolor-in-android
+        onePlayerButton.setBackgroundColor(Color.GREEN);
+
+        onePlayerButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // after user input stop game
+                game.stop(new Player(""));
+                Toast.makeText(OnePlayerActivity.this, "Time has been recorded.", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
 
