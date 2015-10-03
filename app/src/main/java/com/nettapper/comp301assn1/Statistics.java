0b6557003498510fc932f30796f7a1bdaa86caa0
Conner D <connerdunn7399@gmail.com>
2015-10-03 15:55:16 -0600

    Implemented the statistics

--- app/src/main/java/com/nettapper/comp301assn1/Statistics.java
+++ app/src/main/java/com/nettapper/comp301assn1/Statistics.java
@@ -1,6 +1,8 @@
 package com.nettapper.comp301assn1;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 /**
  * Created by Conner on 15-10-01.
@@ -15,15 +17,49 @@ public class Statistics {
         times = new ArrayList<>();
     }
     public Integer minTime(Integer ofLast){
-        return 0;
+        int min = -1;
+        if (times.size() >= 1){
+            min = times.get(0);  // the min should start as the first element
+        }
+        for(int i = 0; i < times.size() && i < ofLast; i++){
+            if(min > times.get(0)){
+                min = times.get(0);
+            }
+        }
+
+        return min;
     }
     public Integer maxTime(Integer ofLast){
-        return 0;
+        int max = -1;
+        if (times.size() >= 1){
+            max = times.get(0);  // the min should start as the first element
+        }
+        for(int i = 0; i < times.size() && i < ofLast; i++){
+            if(max < times.get(0)){
+                max = times.get(0);
+            }
+        }
+
+        return max;
     }
-    public Integer averageTime(Integer ofLast){
-        return 0;
+    public Double averageTime(Integer ofLast){
+        int i = 0;
+        double sum = 0;
+        for(i = 0; i < times.size() && i < ofLast; i++){
+            sum += times.get(i);
+        }
+        return sum / i;
     }
     public Integer medianTime(Integer ofLast){
-        return 0;
+        ArrayList<Integer> copy = new ArrayList<>();
+        for(int i = 0; i < times.size() && i < ofLast; i++){
+            copy.add(times.get(i));
+        }
+        Collections.sort(copy);
+        int med = -1;
+        if(copy.size() >= 1){
+            med = copy.get(copy.size() / 2);
+        }
+        return med;
     }
 }
