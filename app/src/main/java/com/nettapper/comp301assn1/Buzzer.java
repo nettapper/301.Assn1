a6574c3d85b41b93bc299453f3389e0477f8410f
Conner D <connerdunn7399@gmail.com>
2015-10-04 15:50:31 -0600

    Two player buzzer works

--- app/src/main/java/com/nettapper/comp301assn1/Buzzer.java
+++ app/src/main/java/com/nettapper/comp301assn1/Buzzer.java
@@ -4,14 +4,16 @@ package com.nettapper.comp301assn1;
  * Created by Conner on 15-10-01.
  */
 public class Buzzer {
+    Boolean gameRunning = false;
 
     public void start(){
-
+        gameRunning = true;
     }
     public void stop(Player player){
-
+        player.incBuzzerWon();
+        gameRunning = false;
     }
     public void reset(){
-
+        gameRunning = false;
     }
 }
--- app/src/main/java/com/nettapper/comp301assn1/TwoPlayerActivity.java
+++ app/src/main/java/com/nettapper/comp301assn1/TwoPlayerActivity.java
@@ -1,15 +1,77 @@
 package com.nettapper.comp301assn1;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 public class TwoPlayerActivity extends AppCompatActivity {
+    Buzzer buz;
+    ArrayList<Player> players;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_two_player);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        buz = new Buzzer();
+        RecordManager recMan = new RecordManager();
+        try {
+            players = recMan.load(openFileInput("player.sav"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Button one = (Button) findViewById(R.id.twoPlayer_PlayerOne);
+        Button two = (Button) findViewById(R.id.twoPlayer_PlayerTwo);
+
+
+        one.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                buz.stop(players.get(0));
+                displayWinner("Player 1 Won");
+            }
+        });
+
+        two.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                buz.stop(players.get(1));
+                displayWinner("Player 2 Won");
+            }
+        });
+    }
+
+    private void displayWinner(String message) {
+        // Oct 1 2015, http://www.tutorialspoint.com/android/android_alert_dialoges.htm
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage(message);
+
+        alertDialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface arg0, int arg1) {
+                onStart();
+            }
+        });
+
+        alertDialogBuilder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                onStart();
+            }
+        });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+        // End of Tutorialspoint
     }
 }
