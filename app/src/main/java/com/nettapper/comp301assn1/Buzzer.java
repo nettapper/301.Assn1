e0b43a9224ae2a0af5f276eacfd72bac9a61716d
Conner D <connerdunn7399@gmail.com>
2015-10-04 22:38:09 -0600

    Added GPLv3 licence headers

--- app/src/main/java/com/nettapper/comp301assn1/Buzzer.java
+++ app/src/main/java/com/nettapper/comp301assn1/Buzzer.java
@@ -1,3 +1,22 @@
+/*
+    Implements the model for the players to play a buzzer game.
+    Copyright (C) 2015  Conner Dunn
+
+    This file is part of ccdunn-reflex.
+
+    ccdunn-reflex is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ccdunn-reflex is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ccdunn-reflex.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.nettapper.comp301assn1;
 
 /**
